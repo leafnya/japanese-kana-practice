1007d44d224133638d81341e26b195a5f6e1e43d
leafnya <leafnya@outlook.com>
2024-09-29 23:12:38 +0800

    feat: colors

--- app.vue
+++ app.vue
@@ -21,45 +21,37 @@ function choose(option: number) {
 </script>
 
 <template>
-  <div class="flex size-full items-center justify-center">
+  <div class="flex size-full items-center justify-center bg-zinc-900 text-xl text-white">
     <div class="relative">
       <div
-        class="flex size-20 items-center justify-center border"
+        class="flex size-20 items-center justify-center font-medium"
       >
         {{ question.question }}
       </div>
       <OptionCell
-        class="-translate-y-full"
-        :class="{
-          'bg-red-200': wrong && lastOption === 0,
-        }"
+        class="-translate-y-full border-purple-500"
+        :wrong="wrong && lastOption === 0"
         @click="choose(0)"
       >
         {{ question.options[0] }}
       </OptionCell>
       <OptionCell
-        class="translate-x-full"
-        :class="{
-          'bg-red-200': wrong && lastOption === 1,
-        }"
+        class="translate-x-full border-amber-300"
+        :wrong="wrong && lastOption === 1"
         @click="choose(1)"
       >
         {{ question.options[1] }}
       </OptionCell>
       <OptionCell
-        class="translate-y-full"
-        :class="{
-          'bg-red-200': wrong && lastOption === 2,
-        }"
+        class="translate-y-full border-pink-400"
+        :wrong="wrong && lastOption === 2"
         @click="choose(2)"
       >
         {{ question.options[2] }}
       </OptionCell>
       <OptionCell
-        class="-translate-x-full"
-        :class="{
-          'bg-red-200': wrong && lastOption === 3,
-        }"
+        class="-translate-x-full border-lime-400"
+        :wrong="wrong && lastOption === 3"
         @click="choose(3)"
       >
         {{ question.options[3] }}
@@ -74,5 +66,6 @@ html, body, #__nuxt {
   height: 100%;
   margin: 0;
   padding: 0;
+  cursor: default;
 }
 </style>
--- components/OptionCell.vue
+++ components/OptionCell.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const { wrong } = defineProps<{ wrong: boolean }>()
+</script>
+
 <template>
-  <button class="absolute inset-0 size-20 border">
-    <slot />
+  <button
+    class="absolute inset-0 flex size-20 items-center justify-center rounded-xl border-2 font-medium transition-colors"
+    :class="{ 'bg-red-500/80 ': wrong }"
+  >
+    <span :class="{ 'animate-[bounce_0.3s_ease]': wrong }">
+      <slot />
+    </span>
   </button>
 </template>
